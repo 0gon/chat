17300b28c045b60895b218a25fc584157da0161c
0gon <dudrhs4563@naver.com>
2024-06-19 22:25:59 +0900

    커밋

--- src/main/java/com/chat/demo/chat/WebSocketHandler.java
+++ src/main/java/com/chat/demo/chat/WebSocketHandler.java
@@ -21,48 +21,29 @@ public class WebSocketHandler extends TextWebSocketHandler {
     Map<String, WebSocketSession> sessionMap = new HashMap<>(); // 웹소켓 세션을 담아둘 맵
     Map<String, String> userMap = new HashMap<>(); // 사용자
 
+    /* 클라이언트가 소켓 연결시 동작 */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("\n소켓연결. 세션: {}", session.getId());
+        sessionMap.put(session.getId(), session);
+    }
+
     /* 클라이언트로부터 메시지 수신시 동작 */
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String msg = message.getPayload();
-        log.info("===============Message=================");
-        log.info("{}", msg);
-        log.info("===============Message=================");
-
-        JSONObject obj = jsonToObjectParser(msg);
-        // 로그인된 Member (afterConnectionEstablished 메소드에서 session을 저장함)
+        log.info("\n수신메세지: {}", msg);
         for (String key : sessionMap.keySet()) {
             WebSocketSession wss = sessionMap.get(key);
-
-            if (userMap.get(wss.getId()) == null) {
-                userMap.put(wss.getId(), (String) obj.get("userName"));
-            }
-
-            // 클라이언트에게 메시지 전달
-            wss.sendMessage(new TextMessage(obj.toJSONString()));
+            wss.sendMessage(new TextMessage(msg));
         }
-    }
-
-    /* 클라이언트가 소켓 연결시 동작 */
-    @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        log.info("{} 연결되었습니다.", session.getId());
-        super.afterConnectionEstablished(session);
-        sessionMap.put(session.getId(), session);
-
-        JSONObject obj = new JSONObject();
-        obj.put("type", "getId");
-        obj.put("sessionId", session.getId());
-
-        // 클라이언트에게 메세지 전달
-        session.sendMessage(new TextMessage(obj.toJSONString()));
     }
 
     /* 클라이언트가 소켓 종료시 동작 */
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        log.info("{} 연결이 종료되었습니다.", session.getId());
-        super.afterConnectionClosed(session, status);
+        log.info("\n소켓연결 종료. 세션: {}", session.getId());
+        // super.afterConnectionClosed(session, status);
         sessionMap.remove(session.getId());
 
         String userName = userMap.get(session.getId());
